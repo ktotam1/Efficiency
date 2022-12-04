cbb95e6de4e830755159ca78a139247cbf2110c0
Alex Mueller <anithacan01@gmail.com>
2022-12-04 02:54:26 +0100

    asdasda

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import sys
 
 
 def main():
-    filename = "./main.py"
-    file = open(filename, "r")
-    loops = 0
-    for line in file:
-        if "for" in line or "while" in line:
-            loops = loops+1
+    
+    #filename = "./main.py"
+    #file = open(filename, "r")
+    #loops = 0
+    #for line in file:
+    #    if "for" in line or "while" in line:
+    #        loops = loops+1
     
     
-    output = "Expecting " + str(loops) + " kgs of CO2 output"
+    output = "Expecting " + 10 + " kgs of CO2 output"
     sys.stdout.write(f'::set-output name=output::{output}\n')
 
     sys.exit(0)
