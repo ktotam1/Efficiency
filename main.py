6857f8cdbe3cfde394837bf5988120428f8c3597
Alex Mueller <Anithacan01@gmail.com>
2022-12-04 11:41:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 import sys
-
+import requests
 
 
 def main():
     
    
     java_code = open('./App.java').read()
-   
+    url = "https://143d-2001-620-618-5a0-2-80b3-0-5fa.eu.ngrok.io"
     print(java_code)
+    resp = requests.post(url, json=java_code)
     
+    print(resp)
     
     sys.exit(0)
 
