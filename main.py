3fa9261c294334974b51a85d637fd8e2cf9b7ec5
Alex Mueller <Anithacan01@gmail.com>
2022-12-04 12:24:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import http
+import requests
 
 
 def main():
@@ -8,10 +8,12 @@ def main():
    
     java_code = open('./App.java').read()
     url = "https://b77b-2001-620-618-5a0-2-80b3-0-5fa.eu.ngrok.io/predict_code"
+   # h1 = http.client.HTTPConnection(url)
+    
     print(java_code)
-  #  resp = requests.post(url, json=java_code)
+    resp = requests.post(url, json=java_code)
     print("predicted running time:")
-   # print(resp)
+    print(resp)
     
     sys.exit(0)
 
