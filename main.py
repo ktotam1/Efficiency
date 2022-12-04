99786068042501d41de785e10d0fcba25925512d
Alex Mueller <Anithacan01@gmail.com>
2022-12-04 12:33:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     print(java_code)
     resp = requests.post(url, json=java_code)
     print("predicted running time:")
-    print(resp.json())
+    print(resp.content.decode())
     
     sys.exit(0)
 
